b1ea2002c0207ef82dbf3430b8264ec28577851c
Oliver <oliveroneill04@gmail.com>
2016-12-31 18:08:37 +1100

    added robustness to failure upload script

--- Example/Tests/upload_failures.py
+++ Example/Tests/upload_failures.py
@@ -5,9 +5,13 @@ import tinys3
 
 if __name__ == '__main__':
     filename = 'failures_{}'.format(time.time())
-    shutil.make_archive(filename, 'zip', 'FailureDiffs/')
-    filename += '.zip'
+    failureDir = 'FailureDiffs/'
+    if os.path.isdir(failureDir):
+        shutil.make_archive(filename, 'zip', failureDir)
+        filename += '.zip'
 
-    conn = tinys3.Connection(os.environ['AWS_ACCESS_KEY_ID'],os.environ['AWS_SECRET_ACCESS_KEY'],tls=True)
-    f = open(filename,'rb')
-    conn.upload(filename,f,os.environ['UPLOAD_IOS_SNAPSHOT_BUCKET_NAME'])
+        conn = tinys3.Connection(os.environ['AWS_ACCESS_KEY_ID'],os.environ['AWS_SECRET_ACCESS_KEY'],tls=True)
+        with open(filename,'rb') as f:
+            conn.upload(filename,f,os.environ['UPLOAD_IOS_SNAPSHOT_BUCKET_NAME'])
+    else:
+        print("No failures found")
