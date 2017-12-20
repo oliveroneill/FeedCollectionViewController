d2b1c8d655718063545f094a995fe39a9a55dc8e
Oliver <oliveroneill04@gmail.com>
2017-12-21 07:32:25 +1100

    Update travis xcode version

---
+++ FeedCollectionViewController/Classes/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  DACircularProgress
+//
+//  Created by Oliver ONeill on 20/12/17.
+//
+
+import UIKit
+
+class ErrorView: UITextView {
+    override var font = UIFont(name: "Helvetica", size: 32)
+    
+    override var text: String! {
+        didSet {
+            
+        }
+    }
+}
